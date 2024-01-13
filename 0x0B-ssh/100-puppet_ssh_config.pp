7a63e4466fdf21d73a3144730636911ff5cfe3f2
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-01-13 05:47:03 +0200

    automate the config file properly with puppet manifest

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,19 +1,25 @@
 #!/usr/bin/env bash
 # configuration automation manifest
 
-file { 'school_config':
-  ensure  => 'present',
-  path    => '/etc/ssh/ssh_config.d/school_config.conf',
-  content => "Host school_server\n\
+include stdlib  # to use file_line
+
+$file_content = "Host school_server\n\
     HostName 54.234.64.96\n\
     User ubuntu\n\
     IdentityFile ~/.ssh/school\n\
-    PasswordAuthentication no\n",
+    PasswordAuthentication no\n"
+
+
+file { 'school_config':
+  ensure  => 'present',
+  path    => '/etc/ssh/ssh_config.d/school_config.conf',
+  content => $file_content,
+  mode    => '0644',
 }
 
 file_line { 'no_password':
   ensure  => 'present',
   path    => '/etc/ssh/ssh_config.d/school_config.conf',
-  line    => '       PasswordAuthentication no',
+  line    => '    PasswordAuthentication no',
   require => File['school_config'],
 }
