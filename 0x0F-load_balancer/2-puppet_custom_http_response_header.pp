29414e47e3ada4fa5ec56cd335ba07ebeccf09f7
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-01-25 17:40:39 +0200

    Update nginx configuration to add custom HTTP response header and error pages

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,49 +1,48 @@
-#!/usr/bin/env bash
 # configuration automation manifest
 
 $hostname_parts = split($::hostname, '-')
 $server_name = "${hostname_parts[1]}-${hostname_parts[2]}"
 
 package { 'nginx':
-	ensure => installed,
+  ensure => installed,
 }
 
 file { '/etc/nginx/sites-available/default':
-	ensure  => present,
-	content => "server {
-		listen 80;
+  ensure  => present,
+  content => "server {
+    listen 80;
 
-		add_header X-Served-By ${server_name};
+    add_header X-Served-By ${server_name};
 
-		location / {
-				root /var/www/html;
-				index test.html;
-		}
+    location / {
+      root /var/www/html;
+      index test.html;
+    }
 
-		location /redirect_me {
-				rewrite ^/redirect_me/(.*)$ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
-		}
+    location /redirect_me {
+      rewrite ^/redirect_me/(.*)$ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+    }
 
-		error_page 404 /404.html;
-		location = /404.html {
-				root /var/www/html;
-				internal;
-		}
-	}",
+    error_page 404 /404.html;
+    location = /404.html {
+      root /var/www/html;
+      internal;
+    }
+  }",
 }
 
 file { '/var/www/html/test.html':
-	ensure  => present,
-	content => 'Hello World!',
+  ensure  => present,
+  content => 'Hello World!',
 }
 
 file { '/var/www/html/404.html':
-	ensure  => present,
-	content => 'Ceci n\'est pas une page',
+  ensure  => present,
+  content => 'Ceci n\'est pas une page',
 }
 
 service { 'nginx':
-	ensure    => running,
-	enable    => true,
-	subscribe => File['/etc/nginx/sites-available/default'],
+  ensure    => running,
+  enable    => true,
+  subscribe => File['/etc/nginx/sites-available/default'],
 }
